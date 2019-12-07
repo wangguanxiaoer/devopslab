54983772c6cf97993dea1becf13ca051d30c655e
wangguanxiaoer <44286577+wangguanxiaoer@users.noreply.github.com>
2019-12-07 13:46:29 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=wangguanxiaoer
 
 export MYCLUSTER=tektonknativebeijing02
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing02/kube-config-tok04-tektonknativebeijing02.yml
 
 export NAMESPACE=wangguanxiaoer
 export EMAIL=wangguanxiaoer@163.com
