317b707c94e8fe9f0d06253dbc6e6f6ee9021870
wangguanxiaoer <44286577+wangguanxiaoer@users.noreply.github.com>
2019-12-07 13:49:13 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -5,4 +5,4 @@ export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonk
 
 export NAMESPACE=wangguanxiaoer
 export EMAIL=wangguanxiaoer@163.com
-export INGRESS=
+export INGRESS=tektonknativebeijing02-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
