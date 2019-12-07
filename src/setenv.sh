95b1d6ce11e723121e8f22e403b90b15c9624aaa
wangguanxiaoer <44286577+wangguanxiaoer@users.noreply.github.com>
2019-12-07 13:22:09 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=wangguanxiaoer
 
 export MYCLUSTER=
 export KUBECONFIG=
 
 export NAMESPACE=
-export EMAIL=
+export EMAIL=wangguanxiaoer@163.com
 export INGRESS=
